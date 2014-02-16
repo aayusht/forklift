33685676b09a3cd4300e4040a8391a9382a8c55a
Stephen Parish <stephen.parish@docusign.com>
2014-02-16 06:55:27 -0800

    Revert "Make sure we only call executeOnExecutor for API 11 and above."
    This reverts commit 51391deee5a181f30b403af25bfe62fddf15317c.

--- src/com/docusign/dataaccess/AsyncChainLoader.java
+++ src/com/docusign/dataaccess/AsyncChainLoader.java
@@ -3,7 +3,6 @@ package com.docusign.dataaccess;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.SystemClock;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
@@ -201,12 +200,8 @@ public abstract class AsyncChainLoader<T> extends AsyncTaskLoader<Result<T>>
 	public final void onLoadComplete(Loader<Result<T>> loader, Result<T> data) {
 		if (loader != m_Chain)
 			throw new UnsupportedOperationException("ChainAsyncTaskLoader must only handle callbacks for its chained loader.");
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-            mFallbackDeliveredTasks.add(new FallbackDeliveredAsyncTask().execute(data));
-        } else {
-            mFallbackDeliveredTasks.add(new FallbackDeliveredAsyncTask().executeOnExecutor(FallbackDeliveryExecutor.get(this), data));
-        }
+		
+		mFallbackDeliveredTasks.add(new FallbackDeliveredAsyncTask().executeOnExecutor(FallbackDeliveryExecutor.get(this), data));
 	}
 	
 	protected T onFallbackDelivered(T data, Type type) throws DataProviderException {
